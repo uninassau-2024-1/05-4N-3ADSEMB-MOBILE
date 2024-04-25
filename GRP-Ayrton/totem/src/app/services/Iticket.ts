8c9ad9dd30b45536f06627b89929b0755039ff0d
AyrtonF <ayrtonleonardo14@gmail.com>
2024-04-24 22:57:37 -0300

    reenviando codigo(que funcione dessa vez)

---
+++ GRP-Ayrton/totem/src/app/services/Iticket.ts
@@ -0,0 +1,7 @@
+export interface Iticket{
+   nome:string;
+   categoria:string;
+   atendido:boolean;
+   dataEmissao:Date;
+
+}
---
+++ GRP-Ayrton/totem/src/app/tab2/tab2.page.ts
@@ -0,0 +1,17 @@
+import { SenhaService } from 'src/app/services/senha.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(public SenhaService: SenhaService) {}
+
+  chamarProximaSenha() {
+    this.SenhaService.chamarProximaSenha();
+  }
+
+}
