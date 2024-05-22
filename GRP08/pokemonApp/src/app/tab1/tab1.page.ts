09e7a7abc115455cfbeb563c854a83eac642b36e
mariojorge-dev <108435653+mariojorge-dev@users.noreply.github.com>
2024-05-22 19:21:14 -0300

    Add files via upload
    Enviando o projeto pokémon para o melhor professor da Nassau

---
+++ GRP08/pokemonApp/src/app/tab1/tab1.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { PokemonDataService } from '../services/pokemon-data.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  areaBuscarPokemon: string = '';
+  pokemon: any = {}; // Defina a variável pokemon aqui
+
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private pokemonDataService: PokemonDataService
+  ) { }
+
+  buscarPokemon() {
+    this.pokeAPIService.getPokeAPIService(this.gerarIdAleatorio())
+      .subscribe((pokemonData) => {
+        const pokemon = JSON.parse(JSON.stringify(pokemonData));
+        this.pokemonDataService.adicionarPokemonCapturado(pokemon); // Salvando o Pokémon no serviço de dados
+        this.pokemon = pokemon; // Atualize a variável pokemon aqui
+      });
+  }
+
+  gerarIdAleatorio(): number {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+}
