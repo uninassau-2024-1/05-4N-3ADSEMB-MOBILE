eef11ccc4e9a2d4cc0a2f0d59040b720c3105e8d
brunaesena <bruna_ewelin@hotmail.com>
2024-05-29 20:11:28 -0300

    projeto 1

--- GRP05/totem/src/app/tab3/tab3.page.ts
+++ GRP05/totem/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public senhasService: SenhasService) {}
 
 }
+
+// Alunos(a) Contribuintes: João Gabriel Ferreira Mendes | Scrum Master
+// Julia de Andrade Leal
+// Luigi Vinicius Mendes de Albuquerque
+// Regina Rayssa Cordoville
+// Paulo Dion Soares
